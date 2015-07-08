ab991a0745f222dd9866f5410d401632cfce6bf6
Robin Coma Delperier <rcomadelperier@sqli.com>
2015-07-08 12:00:23 +0200

    Latencies issues of async stubs file loading resolved

--- app/appStub.js
+++ app/appStub.js
@@ -1,25 +1,60 @@
 angular.module('appStub', [
     'app',
     'ngMockE2E'
-]).config(function(){
+]).config(function($httpProvider){
 
-}).run(function(settings, $httpBackend, $resource){
+    $httpProvider.interceptors.push('HttpStubInterceptor');
 
-    $httpBackend.whenGET(settings.endpoint + 'users').respond($resource('stub/GET-users.json').query());
+}).run(function(settings, $httpBackend, GetJsonFile){
 
-    $httpBackend.whenGET(settings.endpoint + 'users/1').respond($resource('stub/GET-user-01.json').get());
-    $httpBackend.whenGET(settings.endpoint + 'users/2').respond($resource('stub/GET-user-02.json').get());
-    $httpBackend.whenGET(settings.endpoint + 'users/3').respond($resource('stub/GET-user-03.json').get());
-    $httpBackend.whenGET(settings.endpoint + 'users/4').respond($resource('stub/GET-user-04.json').get());
-    $httpBackend.whenGET(settings.endpoint + 'users/5').respond($resource('stub/GET-user-05.json').get());
-    $httpBackend.whenGET(settings.endpoint + 'users/6').respond($resource('stub/GET-user-06.json').get());
-    $httpBackend.whenGET(settings.endpoint + 'users/7').respond($resource('stub/GET-user-07.json').get());
-    $httpBackend.whenGET(settings.endpoint + 'users/8').respond($resource('stub/GET-user-08.json').get());
-    $httpBackend.whenGET(settings.endpoint + 'users/9').respond($resource('stub/GET-user-09.json').get());
-    $httpBackend.whenGET(settings.endpoint + 'users/10').respond($resource('stub/GET-user-10.json').get());
+    $httpBackend.whenGET(settings.endpoint + 'users').respond(GetJsonFile.synchronously('stub/GET-users.json'));
+
+    $httpBackend.whenGET(settings.endpoint + 'users/1').respond(GetJsonFile.synchronously('stub/GET-user-01.json'));
+    $httpBackend.whenGET(settings.endpoint + 'users/2').respond(GetJsonFile.synchronously('stub/GET-user-02.json'));
+    $httpBackend.whenGET(settings.endpoint + 'users/3').respond(GetJsonFile.synchronously('stub/GET-user-03.json'));
+    $httpBackend.whenGET(settings.endpoint + 'users/4').respond(GetJsonFile.synchronously('stub/GET-user-04.json'));
+    $httpBackend.whenGET(settings.endpoint + 'users/5').respond(GetJsonFile.synchronously('stub/GET-user-05.json'));
+    $httpBackend.whenGET(settings.endpoint + 'users/6').respond(GetJsonFile.synchronously('stub/GET-user-06.json'));
+    $httpBackend.whenGET(settings.endpoint + 'users/7').respond(GetJsonFile.synchronously('stub/GET-user-07.json'));
+    $httpBackend.whenGET(settings.endpoint + 'users/8').respond(GetJsonFile.synchronously('stub/GET-user-08.json'));
+    $httpBackend.whenGET(settings.endpoint + 'users/9').respond(GetJsonFile.synchronously('stub/GET-user-09.json'));
+    $httpBackend.whenGET(settings.endpoint + 'users/10').respond(GetJsonFile.synchronously('stub/GET-user-10.json'));
 
     $httpBackend.whenGET(/.*/).passThrough();
     $httpBackend.whenPOST(/.*/).passThrough();
     $httpBackend.whenDELETE(/.*/).passThrough();
     $httpBackend.whenPUT(/.*/).passThrough();
+});
+
+angular.module('appStub').service('HttpStubInterceptor', function($q, $timeout){
+    var getMockedAsyncRespondTime = function (url) {
+        switch (url.split(/\./).pop()) {
+            case 'json':
+                return 300;
+            case 'html':
+                // In production all views are into cachedUrl as JS Templates
+                return 0;
+            default:
+                // Web Services
+                return 1000;
+        }
+    };
+    return {
+        response: function (response) {
+            var defer = $q.defer();
+            $timeout(function () {
+                defer.resolve(response);
+            }, getMockedAsyncRespondTime(response.config.url.toString()));
+            return defer.promise;
+        }
+    };
+});
+
+angular.module('appStub').service('GetJsonFile', function(){
+    this.synchronously = function(url){
+        var request = new XMLHttpRequest();
+        request.open('GET', url, false);
+        request.send(null);
+        return request.response;
+    };
 });
